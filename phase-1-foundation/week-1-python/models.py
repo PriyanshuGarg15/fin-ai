a3f86ea2250d6d9939f188f87693dd0cd0f27a2e
PriyanshuGarg <priyanshu.159803@gmail.com>
2026-04-20 22:33:50 +0530

    adding models

---
+++ phase-1-foundation/week-1-python/models.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field, field_validator
+from enum import Enum
+from typing import Optional
+
+class LoanType(str, Enum):
+    PERSONAL = "personal"
+    NEWCAR = "newcar"
+    USEDCAR = "usedcar"
+
+class LoanApplicationRequest(BaseModel):
+    leadId: str = Field(..., description="Unique identifier for the loan application")
+    loan_type: LoanType
+    loan_amount: float =Field(..., gt=0, le=1000000)
+    monthly_income: float =Field(..., gt=0 )
+    credit_score: float = Field(..., gt=300, le=850)
+
+    @field_validator("loan_amount")
+    @classmethod
+    def loan_amount_limit(cls, v, info):
+        if 'monthly_income' in info.data:
+            if v > (60 * info.data.monthly_income):
+                raise ValueError("Loan amount must not be more than 60x of monthly income")
+        return v
+
+class loanApplicationResponse(BaseModel):
+    leadId: str = Field(..., description="Unique identifier for the loan application")
